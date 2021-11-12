08926f3d9b142577c6a5b526d5e33e3c341783ad
ejdaoDev <ejdao2021@gmail.com>
2021-11-12 01:54:18 -0500

    se agregan cambios realizados previamente a la pagina

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -44,9 +44,11 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="assets/dashboard/vendor/jquery/jquery.min.js"></script>
+        <script src="assets/dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="js/app.js"></script>
         <!-- Datatables -->
-        <script src="assets/dashboard/vendor/jquery/jquery.min.js"></script>
+       
         <link rel="stylesheet" href="assets/dashboard/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
         <link rel="stylesheet" href="assets/dashboard/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
         <link rel="stylesheet" href="assets/dashboard/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
